535bc4f320c0d1644a9f972e0f8866cefff4638c
leozhou <492753662@qq.com>
2016-03-14 23:28:52 +0800

    ajax 提交表单

--- meadowlark.js
+++ meadowlark.js
@@ -40,11 +40,18 @@ app.get('/newsletter',function(req,res){
 });
 
 app.post('/process',function(req,res){
-	console.log('Form(from querystring): '+req.query.form);
-	console.log('CSRF token (from hidden form field): '+ req.body._csrf);
-	console.log('Name (from visible form field): '+ req.body.name);
-	console.log('Email (from visible form field: '+ req.body.email);
-	res.redirect(303,'/thank-you');
+	if (req.xhr || req.accepts('json,html')==='json') {
+		//如果发生错误，应该发送{error: 'error description'}
+		res.send({success:true});
+	}else{
+		//如果发生错误，应该重定向到错误页面
+		console.log('Form(from querystring): '+req.query.form);
+		console.log('CSRF token (from hidden form field): '+ req.body._csrf);
+		console.log('Name (from visible form field): '+ req.body.name);
+		console.log('Email (from visible form field: '+ req.body.email);
+		res.redirect(303,'/thank-you');	
+	}
+	
 });
 
 //定制404页面
