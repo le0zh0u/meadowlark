46eedd09d4ad5a8b7650f46e7e63ac10c40d40cf
leozhou <492753662@qq.com>
2016-03-14 23:07:12 +0800

    添加普通的form表单提交
    /newsletter

--- meadowlark.js
+++ meadowlark.js
@@ -34,6 +34,19 @@ app.get('/tours/request-group-rate',function(req, res){
 	res.render('tours/request-group-rate');
 });
 
+app.use(require('body-parser')());
+app.get('/newsletter',function(req,res){
+	res.render('newsletter',{csrf:'CSRF token goes here'});
+});
+
+app.post('/process',function(req,res){
+	console.log('Form(from querystring): '+req.query.form);
+	console.log('CSRF token (from hidden form field): '+ req.body._csrf);
+	console.log('Name (from visible form field): '+ req.body.name);
+	console.log('Email (from visible form field: '+ req.body.email);
+	res.redirect(303,'/thank-you');
+});
+
 //定制404页面
 app.use(function(req, res, next){
 	res.status(404);
