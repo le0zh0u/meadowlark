2b0fd50557294413653cbcefd2ebe039409b6d15
leozhou <492753662@qq.com>
2016-03-17 22:40:07 +0800

    finish session

--- meadowlark.js
+++ meadowlark.js
@@ -20,6 +20,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(require('cookie-parser')(credentials.cookieSecret));
 app.use(require('express-session')());
 
+app.use(function(req, res, next){
+	//如果有即显消息，把它传到上下文中，然后清除它
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+})
+
 app.get('/', function(req, res){
 	res.render('home');
 });
@@ -40,7 +47,37 @@ app.get('/tours/request-group-rate',function(req, res){
 
 app.use(require('body-parser')());
 app.get('/newsletter',function(req,res){
-	res.render('newsletter',{csrf:'CSRF token goes here'});
+	var name = req.body.name || '', email = req.body.email || '';
+	//输入验证
+	var VALID_EMAIL_REGEX = "dfasdf";
+	if (!email.match(VALID_EMAIL_REGEX)) {
+		if (req.xhr) {return res.json({ error: 'Invalid name email address.'});};
+		req.session.flash = {
+			type:'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was not valid.',
+		};
+		return res.redirect(303,'/newsletter/archive');
+	};
+	new NewsletterSignup({name:name, email: email}).save(function(err){
+		if (err) {
+			if (req.xhr) {return res.json({error: 'Database error.'});};
+			req.session.flash = {
+				type:'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.',
+			}
+			return res.redirect(303,'/newsletter/archive');
+		};
+		if (req.xhr) {return res.json({success: true});};
+		req.session.flash = {
+			type:'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303,'/newsletter/archive');
+	});
+	// res.render('newsletter',{csrf:'CSRF token goes here'});
 });
 
 app.post('/process',function(req,res){
